74478f44b02be581208f649c294c6ecabf9a4c91
CodingRatio <coding.project1.618@gmail.com>
2022-07-21 11:40:33 -0500

    Returns the intials of a persons full name

---
+++ abbreviateATwoWordName.js
@@ -0,0 +1,19 @@
+/*
+Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+The output should be two capital letters with a dot separating them.
+
+It should look like this:
+
+Sam Harris => S.H
+
+patrick feeney => P.F
+*/
+
+//Answer
+function abbrevName(name) {
+  return name
+    .split(" ")
+    .map((el) => el.substring(0, 1).toUpperCase())
+    .join(".");
+}
